892ad2e617462b7dc0854a8f76e9f7b176139b8e
fred25 <fredericomattos25@gmail.com>
2026-05-24 19:08:14 -0300

    begin player file

---
+++ src/entities/player.c
@@ -0,0 +1,13 @@
+// inclusão de bibliotecas
+#include "player.h"
+
+/**
+ * Função com a lógica do player
+ * recebe um objeto player e devolve uma versão atualizada
+ * para o próximo frame
+ */
+PLAYER player_logic(PLAYER player){
+    //TODO
+    
+    return player;
+}
---
+++ src/entities/player.h
@@ -0,0 +1,11 @@
+// estruturas
+typedef struct {
+    int x;
+    int y;
+    float angle;
+    int speed;
+    int vida;
+} PLAYER;
+
+// funcoes
+PLAYER player_logic(PLAYER);
