800c34a05e6aa04978847a1720d8253deda87b8f
fred25 <fredericomattos25@gmail.com>
2026-05-31 17:15:40 -0300

    add bullet logic

--- src/entities/bullet.h
+++ src/entities/bullet.h
@@ -1,4 +1,5 @@
 #include "definitions.h"
+#include <math.h>
 #include "raylib.h"
 
 // define bullet struct
@@ -8,3 +9,7 @@ typedef struct{
 } BULLET;
 
 // define funcitons
+void bullet_logic(BULLET*, int);
+void single_bullet_logic(BULLET*);
+POSITION new_position(BULLET);
+void draw_bullet(BULLET*, Texture2D sprite);
